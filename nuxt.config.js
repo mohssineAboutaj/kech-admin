f05acee0524e314eb2ddbd658418f0b53a720325
mohssineaboutaj <mohssineaboutaj1995@gmail.com>
2021-05-09 03:36:17 +0000

    enable babel config

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,8 +84,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    // babel: {
-    //   plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
-    // },
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
   },
 }
